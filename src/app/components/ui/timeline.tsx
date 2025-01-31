0267a9f82ae4a7bfe203aa951f6d6623898914bc
thelonewolf <thelonewolf06@proton.me>
2025-01-31 23:12:23 +0800

    change the title of the timelineui

--- src/app/components/ui/timeline.tsx
+++ src/app/components/ui/timeline.tsx
@@ -33,17 +33,15 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
   const opacityTransform = useTransform(scrollYProgress, [0, 0.1], [0, 1]);
 
   return (
-    <div
-      className="w-full dark:bg-black font-sans md:px-10"
-      ref={containerRef}
-    >
+    <div className="w-full dark:bg-black font-sans md:px-10" ref={containerRef}>
       <div className="w-full mx-auto text-center py-20 px-4 md:px-8 lg:px-10 ">
         <h2 className="text-lg md:text-4xl mb-4 text-center font-extrabold text-transparent bg-gradient-to-r from-amber-400 to-amber-800 bg-clip-text ">
-          Wanna see more Designs I made?
+          🚀 Experience
         </h2>
         <p className="text-neutral-700 dark:text-neutral-300 text-sm md:text-base text-center">
-          I&apos;ve been working on Aceternity for the past 2 years. Here&apos;s
-          a timeline of my journey.
+          I&apos;ve explored and gained experience in various fields, working with
+          different companies and studying diverse disciplines. <br></br>Let&apos;s take a
+          journey through my professional and academic timeline.
         </p>
       </div>
 
@@ -57,13 +55,13 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
               <div className="h-10 absolute left-3 md:left-3 w-10 rounded-full bg-white dark:bg-black flex items-center justify-center">
                 <div className="h-4 w-4 rounded-full bg-neutral-200 dark:bg-neutral-800 border border-neutral-300 dark:border-neutral-700 p-2" />
               </div>
-              <h3 className="hidden md:block text-xl md:pl-20 md:text-5xl font-bold text-neutral-500 dark:text-neutral-500 ">
+              <h3 className="hidden md:block text-xl md:pl-20 mt-5 md:text-5xl font-bold text-neutral-500 dark:text-neutral-500 ">
                 {item.title}
               </h3>
             </div>
 
             <div className="relative pl-20 pr-4 md:pl-4 w-full">
-              <h3 className="md:hidden block text-2xl mb-4 text-left font-bold text-neutral-500 dark:text-neutral-500">
+              <h3 className="md:hidden block text-2xl mb-4 mt-10 text-left font-bold text-neutral-500 dark:text-neutral-500">
                 {item.title}
               </h3>
               {item.content}{" "}
