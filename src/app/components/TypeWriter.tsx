312582fb15e88a904dff0b76b27c50e40707895a
thelonewolf <thelonewolf06@proton.me>
2025-01-31 23:20:23 +0800

    change the typewriter text

--- src/app/components/TypeWriter.tsx
+++ src/app/components/TypeWriter.tsx
@@ -9,7 +9,7 @@ const TypeWriter = () => {
       <div className="bg-gradient-to-l from-orange-700 to-orange-400 bg-clip-text text-transparent">
         <TypewriterComponent
           options={{
-            strings: ["Irasema", "a Cutie"],
+            strings: ["Irasema", "a Designer"],
             autoStart: true,
             loop: true,
             delay: 75,
